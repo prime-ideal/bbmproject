c2293dd2b4dde7ddd4fcfa9020a2fdca2a21252b
prime-ideal <mtsmts2013@163.com>
2022-03-08 10:51:21 +0800

    set bomb which can not explode with an undone timer

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <queue>
+#include <vector>
 using std::cin;
 using std::cout;
+using std::priority_queue;
+using std::vector;
 
-long long tick = 0;
+long long cnt = 0;
+const int TICK = CLOCKS_PER_SEC / 20;//ÖÂ¾´mc
+clock_t clocktime;
 char** mpt = 0, **ppt;
 float VELOCITY = 0.5;
-const int MAX_LEVEL = 1;
+const int MAX_LEVEL = 2;
 
 
 class map
@@ -41,22 +47,45 @@ private:
 	char name;
 	int posx, posy;
 	float speed, remainx, remainy;
+	int hp;
+	int power;
 public:
 	friend void init(map&);
+	friend void display();
+	friend void setbomb(mob&);
 	mob(int x, int y) : posx(x), posy(y),
-		speed(VELOCITY), remainx(0), remainy(0) {}
+		speed(VELOCITY), remainx(0), remainy(0), 
+		hp(1000), power(1) {}
 	mob() : posx(0), posy(0) {}
 	bool move_up(float);
 	bool move_left(float);
 	bool move_down(float);
 	bool move_right(float);
 };
+class bomb {
+private:
+	int posx, posy;
+	int power;
+	int triggertime;
+public:
+	friend void setbomb(mob&);
+	bool operator<(const bomb& a) const{
+		return this->triggertime > a.triggertime; //Ð¡¶¥¶Ñ
+	}
+	bomb(int x, int y, int p, int t) :
+		posx(x), posy(y), power(p), triggertime(t) {}
+};
+
+priority_queue<bomb>eventlist;
+
+
 void init(map&);
 void display();
 inline bool check(char ch);
 void deal_with_input();
 map gamemap(-1);
-mob player1, player2, mob1, mob2;
+mob player1(0,0), player2(0,0), 
+	mob1(0,0), mob2(0,0);
 
 
 int main()
@@ -71,9 +100,11 @@ int main()
 	display();
 	while (1)
 	{
-
+		clocktime = clock();
 		deal_with_input();
 		display();
+		while (clock() - clocktime <= TICK*2);
+		++cnt;
 	}
 	return 0;
 }
@@ -117,6 +148,10 @@ void init(map& gamemap)
 void display()
 {
 	system("cls");
+	ppt[mob2.posx][mob2.posy] = mob2.name;
+	ppt[mob1.posx][mob1.posy] = mob1.name;
+	ppt[player2.posx][player2.posy] = player2.name;
+	ppt[player1.posx][player1.posy] = player1.name;
 	for (int i = 1; i <= gamemap.userow(); ++i) {
 		for (int j = 1; j <= gamemap.usecol(); ++j) {
 			if (ppt[i][j])
@@ -145,57 +180,79 @@ void deal_with_input()
 		case 'd':
 			player1.move_right(VELOCITY);
 			break;
+		case 'i':
+			player2.move_up(VELOCITY);
+			break;
+		case 'j':
+			player2.move_left(VELOCITY);
+			break;
+		case 'k':
+			player2.move_down(VELOCITY);
+			break;
+		case 'l':
+			player2.move_right(VELOCITY);
+			break;
+		case ' ':
+			setbomb(player1);
+			break;
+		case 0x0D:
+			setbomb(player2);
+			break;
 		}
 	}
 }
 bool mob::move_up(float sp)
 {
+	if (hp <= 0) return 0;
 	remainx -= sp;
 	if (remainx > -1) return true;
 	remainx += 1;
 	if (check(mpt[posx - 1][posy])) {
-		mpt[posx][posy] = ' ';
+		ppt[posx][posy] = 0;
 		--posx;
-		mpt[posx][posy] = name;
+		ppt[posx][posy] = name;
 		return 1;
 	}
 	else return 0;
 }
 bool mob::move_left(float sp)
 {
+	if (hp <= 0) return 0;
 	remainy -= sp;
 	if (remainy > -1) return true;
 	remainy += 1;
 	if (check(mpt[posx][posy - 1])) {
-		mpt[posx][posy] = ' ';
+		ppt[posx][posy] = 0;
 		--posy;
-		mpt[posx][posy] = name;
+		ppt[posx][posy] = name;
 		return 1;
 	}
 	else return 0;
 }
 bool mob::move_down(float sp)
 {
+	if (hp <= 0) return 0;
 	remainx += sp;
 	if (remainx < 1) return true;
 	remainx -= 1;
 	if (check(mpt[posx + 1][posy])) {
-		mpt[posx][posy] = ' ';
+		ppt[posx][posy] = 0;
 		++posx;
-		mpt[posx][posy] = name;
+		ppt[posx][posy] = name;
 		return 1;
 	}
 	else return 0;
 }
 bool mob::move_right(float sp)
 {
+	if (hp <= 0) return 0;
 	remainy += sp;
 	if (remainy < 1) return true;
 	remainy -= 1;
 	if (check(mpt[posx][posy + 1])) {
-		mpt[posx][posy] = ' ';
+		ppt[posx][posy] = 0;
 		++posy;
-		mpt[posx][posy] = name;
+		ppt[posx][posy] = name;
 		return 1;
 	}
 	else return 0;
@@ -204,4 +261,11 @@ inline bool check(char ch) {
 	if (ch == 'H' || ch == '*' || ch == 'o')
 		return 0;
 	return 1;
+}
+void setbomb(mob& gamemob)
+{
+	int x = gamemob.posx,
+		y = gamemob.posy;
+	mpt[x][y] = 'o';
+	eventlist.push(bomb(x, y, gamemob.power, cnt + 3 * 20 * TICK));
 }
