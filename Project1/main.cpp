185a776b1e6f6599f327956c9e61229dfbbc2fb7
prime-ideal <mtsmts2013@163.com>
2022-03-20 15:08:53 +0800

    修复1个炸弹bug，更新得分系统

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,9 +12,10 @@ using std::vector;
 
 long long cnt = 0;
 const int TICK = CLOCKS_PER_SEC / 20;//致敬mc
-clock_t clocktime;
+clock_t clocktime, clocktime0;
 char** mpt = 0, **ppt;
 float VELOCITY = 0.5;
+const int TOTAL_TIME = 300;//倒计时5分钟
 const int MAX_LEVEL = 2;
 const int DEFAULTHP = 1000;//扣血
 const float BONUS_P = 1;//出道具概率
@@ -22,6 +23,10 @@ const float SPEEDUP_P = 0.5;//道具加速概率
 const float POWERUP_P = 0.5;//道具炸弹增幅概率
 const int effect_ticks = 20 * 20;
 const int INF = 0x3f3f3f3f;
+const int MOVESCORE = 1;//移动得分
+const int BONUSSCORE = 100;//吃道具得分
+const int SETBOMBSCORE = 10;//放炸弹得分
+const int EXPLODESCORE = 10000;//炸敌人得分
 
 class map
 {
@@ -56,20 +61,28 @@ private:
 	int hp;
 	int power;
 	long long speedtime, powertime;
+	long long score;
 public:
-	friend void trykill(mob& gamemob, int x, int y);
+	friend bool trykill(mob& gamemob, int x, int y);
 	friend void init(map&);
-	friend void display();
+	friend bool display();
 	friend void setbomb(mob*);
 	mob(int x, int y) : posx(x), posy(y),
 		speed(VELOCITY), remainx(0), remainy(0), 
-		hp(DEFAULTHP), power(2), speedtime(0), powertime(0) {}
+		hp(DEFAULTHP), power(2), speedtime(0), powertime(0), score(0) {}
 	mob() : posx(0), posy(0) {}
 	long long usespeedtime() const { return speedtime; }
 	long long usepowertime() const { return powertime; }
+	long long usescore() const { return score; }
+	char usename() const { return name; }
+	int usehp() const { return hp; }
+	int usex() const { return posx; }
+	int usey() const { return posy; }
 	void setpower(int x) { power = x; }
 	void setspeed(float x) { speed = x; }
 	void sethp(int x) { hp = x; }
+	void setscore(long long x) { score = x; }
+	void addscore(long long x) { score += x; }
 	bool move_up();
 	bool move_left();
 	bool move_down();
@@ -99,7 +112,7 @@ priority_queue<bomb>eventlist;
 
 
 void init(map&);
-void display();
+bool display();
 inline bool check(char ch);
 void deal_with_input();
 void deal_with_eventlist();
@@ -112,24 +125,24 @@ mob player1(0,0), player2(0,0),
 int main()
 {
 	srand(clock());
-	/*
 	int lev;
 	cout << "请输入关卡数：\n";
 	cin >> lev;
 	if (lev > MAX_LEVEL || lev <= 0)
 		lev = 1;
-	sleep(1000);
-	*/
-	gamemap = map(2);
+	clocktime = clocktime0 = clock();
+	gamemap = map(lev);
 	init(gamemap);
 	display();
 	while (1)
 	{
 		clocktime = clock();
 		deal_with_input();
-		display();
+		if (display()) break;
 		deal_with_eventlist();
 	}
+	while (!eventlist.empty()) eventlist.pop();
+	cnt = 0;
 	return 0;
 }
 
@@ -169,9 +182,19 @@ void init(map& gamemap)
 	ppt[mob1.posx][mob2.posy] = 'S';
 	ppt[mob2.posx][mob2.posy] = 'T';
 }
-void display()
+bool display()
 {
 	system("cls");
+	if (mob1.hp <= 0 && mob2.hp <= 0) {
+		cout << "政清人和";
+		Sleep(2000);
+		return 1;
+	}
+	if (player1.hp <= 0 && player2.hp <= 0) {
+		cout << "和這群蟲豸在一起，怎能搞好炸弹呢！！";
+		Sleep(2000);
+		return 1;
+	}
 	if (mob2.hp >= 0)
 	ppt[mob2.posx][mob2.posy] = mob2.name;
 	if (mob1.hp >= 0)
@@ -193,7 +216,8 @@ void display()
 	}
 	cout << std::endl;
 	if (player1.hp > 0) {
-		cout << "玩家A" << " 血量:" << player1.hp;
+		cout << "玩家A" << " 血量:" << player1.hp
+			<< " 得分：" << player1.usescore();
 		if (player1.speedtime > cnt)
 			cout << "  速度提升";
 		if (player1.powertime > cnt)
@@ -203,7 +227,8 @@ void display()
 		cout << "玩家A RIP";
 	cout << std::endl;
 	if (player2.hp > 0) {
-		cout << "玩家B" << " 血量:" << player2.hp;
+		cout << "玩家B" << " 血量:" << player2.hp
+			<< " 得分：" << player2.usescore();
 		if (player2.speedtime > cnt)
 			cout << "  速度提升";
 		if (player2.powertime > cnt)
@@ -211,6 +236,12 @@ void display()
 	}
 	else
 		cout << "玩家B RIP";
+	cout << std::endl;
+	int a = TOTAL_TIME-(clock() - clocktime0) / CLOCKS_PER_SEC;
+	cout << "剩余时间： " << '0' << a / 60 << ":";
+	if (a % 60 < 10) cout << '0';
+	cout << a % 60;
+	return 0;
 }
 
 void deal_with_input()
@@ -232,24 +263,27 @@ void deal_with_input()
 			player2.move_down();
 		if (GetAsyncKeyState('L'))
 			player2.move_right();
-		if (GetAsyncKeyState(VK_SPACE))
-			setbomb(&player1);
-		if (GetAsyncKeyState(VK_RETURN))
-			setbomb(&player2);
+		if (GetAsyncKeyState(VK_SPACE) && player1.usehp() > 0)
+			if (mpt[player1.usex()][player1.usey()] != 'o')
+				setbomb(&player1);
+		if (GetAsyncKeyState(VK_RETURN) && player2.usehp() > 0)
+			if (mpt[player2.usex()][player2.usey()] != 'o')
+				setbomb(&player2);
 		if (GetAsyncKeyState('1')) {//一键开挂
-			player1.sethp(INF);
-			player2.sethp(INF);
+			if (player1.usehp() > 0) player1.sethp(INF);
+			if (player2.usehp() > 0) player2.sethp(INF);
 		}
-		if (GetAsyncKeyState('2')) {//关闭外挂
-			player1.sethp(DEFAULTHP);
-			player2.sethp(DEFAULTHP);
+		if (GetAsyncKeyState('2')) {//一键回血
+			if (player1.usehp() > 0) player1.sethp(DEFAULTHP);
+			if (player2.usehp() > 0) player2.sethp(DEFAULTHP);
 		}
 	}
 }
 void deal_with_eventlist() {
 	while (!eventlist.empty() && eventlist.top().usetime() <= cnt) {
-		if (eventlist.top().usepower() > 0)
+		if (eventlist.top().usepower() > 0) {
 			explode(eventlist.top());
+		}
 		else if (eventlist.top().usepower() == -1 &&
 			eventlist.top().usetime() >= eventlist.top().usemobpt()->usespeedtime()) {
 			eventlist.top().usemobpt()->setspeed(VELOCITY);//速度提升
@@ -279,6 +313,7 @@ bool mob::move_up()
 		ppt[posx][posy] = 0;
 		--posx;
 		ppt[posx][posy] = name;
+		score += MOVESCORE;
 		return 1;
 	}
 	else if (mpt[posx - 1][posy] == '?') {
@@ -296,6 +331,7 @@ bool mob::move_up()
 		--posx;
 		ppt[posx][posy] = name;
 		mpt[posx][posy] = ' ';
+		score += BONUSSCORE;
 		return 1;
 	}
 	else return 0;
@@ -310,6 +346,7 @@ bool mob::move_left()
 		ppt[posx][posy] = 0;
 		--posy;
 		ppt[posx][posy] = name;
+		score += MOVESCORE;
 		return 1;
 	}
 	else if (mpt[posx][posy - 1] == '?') {
@@ -327,6 +364,7 @@ bool mob::move_left()
 		--posy;
 		ppt[posx][posy] = name;
 		mpt[posx][posy] = ' ';
+		score += BONUSSCORE;
 		return 1;
 	}
 	else return 0;
@@ -341,6 +379,7 @@ bool mob::move_down()
 		ppt[posx][posy] = 0;
 		++posx;
 		ppt[posx][posy] = name;
+		score += MOVESCORE;
 		return 1;
 	}
 	else if (mpt[posx + 1][posy] == '?') {
@@ -358,6 +397,7 @@ bool mob::move_down()
 		++posx;
 		ppt[posx][posy] = name;
 		mpt[posx][posy] = ' ';
+		score += BONUSSCORE;
 		return 1;
 	}
 	else return 0;
@@ -372,6 +412,7 @@ bool mob::move_right()
 		ppt[posx][posy] = 0;
 		++posy;
 		ppt[posx][posy] = name;
+		score += MOVESCORE;
 		return 1;
 	}
 	else if (mpt[posx][posy + 1] == '?') {
@@ -389,6 +430,7 @@ bool mob::move_right()
 		++posy;
 		ppt[posx][posy] = name;
 		mpt[posx][posy] = ' ';
+		score += BONUSSCORE;
 		return 1;
 	}
 	else return 0;
@@ -400,16 +442,21 @@ inline bool check(char ch) {
 }
 void setbomb(mob* gamemob)
 {
+	if (gamemob->hp <= 0 || gamemob->score == 0) return;
+	if (clock() - clocktime0 <= CLOCKS_PER_SEC) return;
 	int x = gamemob->posx,
 		y = gamemob->posy;
 	mpt[x][y] = 'o';
 	eventlist.push(bomb(gamemob, x, y, gamemob->power, cnt + 3 * 20));
+	gamemob->score += SETBOMBSCORE;
 }
 void explode(const bomb& gamebomb) 
 {
 	int const power = gamebomb.power,
 		const x = gamebomb.posx,
 		const y = gamebomb.posy;
+	char ch = gamebomb.usemobpt()->usename();
+	int res = 0;
 	if (power > 0) {
 		mpt[x][y] = ' ';
 		for (int i = x; i > 0 && i > x - power; --i) {
@@ -425,10 +472,11 @@ void explode(const bomb& gamebomb)
 				else ++mpt[i][y];
 				eventlist.push(bomb(&player1, i, y, -3, cnt + 20));//火焰1秒
 			}
-			trykill(player1, i, y);
-			trykill(player2, i, y);
-			trykill(mob1, i, y);
-			trykill(mob2, i, y);
+			res+=
+			(trykill(player1, i, y) +
+				trykill(player2, i, y) +
+				trykill(mob1, i, y) +
+				trykill(mob2, i, y))* EXPLODESCORE;
 		}
 		for (int i = x + 1; i <= gamemap.userow() && i < x + power; ++i) {
 			if (mpt[i][y] != '*' && !check(mpt[i][y]) && mpt[i][y] < 'x') break;
@@ -443,10 +491,11 @@ void explode(const bomb& gamebomb)
 				else ++mpt[i][y];
 				eventlist.push(bomb(&player1, i, y, -3, cnt + 20));
 			}
-			trykill(player1, i, y);
-			trykill(player2, i, y);
-			trykill(mob1, i, y);
-			trykill(mob2, i, y);
+			res +=
+				(trykill(player1, i, y) +
+					trykill(player2, i, y) +
+					trykill(mob1, i, y) +
+					trykill(mob2, i, y)) * EXPLODESCORE;
 		}
 		for (int i = y - 1; i > 0 && i > y - power; --i) {
 			if (mpt[x][i] != '*' && !check(mpt[x][i]) && mpt[x][i] < 'x') break;
@@ -461,10 +510,11 @@ void explode(const bomb& gamebomb)
 				else ++mpt[x][i];
 				eventlist.push(bomb(&player1, x, i, -3, cnt + 20));
 			}
-			trykill(player1, x, i);
-			trykill(player2, x, i);
-			trykill(mob1, x, i);
-			trykill(mob2, x, i);
+			res +=
+				(trykill(player1, x, i) +
+					trykill(player2, x, i) +
+					trykill(mob1, x, i) +
+					trykill(mob2, x, i))* EXPLODESCORE;
 		}
 		for (int i = y + 1; i < gamemap.usecol() && i < y + power; ++i) {
 			if (mpt[x][i] != '*' && !check(mpt[x][i]) && mpt[x][i] < 'x') break;
@@ -479,14 +529,19 @@ void explode(const bomb& gamebomb)
 				else ++mpt[x][i];
 				eventlist.push(bomb(&player1, x, i, -3, cnt + 20));
 			}
-			trykill(player1, x, i);
-			trykill(player2, x, i);
-			trykill(mob1, x, i);
-			trykill(mob2, x, i);
+			res +=
+				(trykill(player1, x, i) +
+					trykill(player2, x, i) +
+					trykill(mob1, x, i) +
+					trykill(mob2, x, i))* EXPLODESCORE;
 		}
 	}
+	if (ch == 'A') player1.addscore(res);
+	if (ch == 'B') player2.addscore(res);
+	if (ch == 'S') mob1.addscore(res);
+	if (ch == 'T') mob2.addscore(res);
 }
-void trykill(mob& gamemob, int x, int y) {
+bool trykill(mob& gamemob, int x, int y) {
 	if (gamemob.posx == x && gamemob.posy == y) {
 		gamemob.hp -= DEFAULTHP;
 		if (gamemob.hp <= 0) {
@@ -495,5 +550,8 @@ void trykill(mob& gamemob, int x, int y) {
 			gamemob.posx = 0;
 			gamemob.posy = 0;
 		}
+		return 1;
 	}
+	else
+		return 0;
 }
